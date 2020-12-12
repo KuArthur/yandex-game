74ecf02616268da20593db5711d05884515a8a59
KuArthur <48932889+KuArthur@users.noreply.github.com>
2020-12-13 00:38:30 +0300

    добавил возможность выбора поля.

--- game.js
+++ game.js
@@ -1,57 +1,60 @@
 const fs = require('fs'); // для работы с файлами
+const readline = require('readline'); //для ввода данных в консоли
 
-function getRandomInt(max) {
+const arrFromFile = getDataFromFile(); //массив, прочитанный из файла
+const arrRand = getRandomArray(); // рандомно сгенерированный массив
+
+// рандомное число
+function getRandomNumber(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
+// рандомный массив
 function getRandomArray() {
   const arr = [];
   
-  const M = getRandomInt(10);
-  const N = getRandomInt(10);
+  const M = getRandomNumber(10);
+  const N = getRandomNumber(10);
 
   for(let i = 0; i < M; i++) {
     arr[i] = [];
 
     for(let j = 0; j < N; j++) {
-        arr[i][j] = getRandomInt(2);
+        arr[i][j] = getRandomNumber(2);
     }
   }
   return arr;
 }
 
-
-
+//подсчет соседей каждого элемента массива
 function countNeighbors(arr,i,j) {
     let count = 0;
     for(let a = i - 1; a <= i + 1; a++) {
         for(let b = j - 1; b <= j + 1; b++) {
             if(!(a < 0 || a > arr.length - 1 || b < 0 || b > arr[a].length - 1 || (a === i && b === j))) {
-                // console.log(arr[a][b]);
                 count += arr[a][b]
             }
         }
     }
-
     return count;
 }
 
+// получение данных из файла
 function getDataFromFile() {
   let array = fs.readFileSync('default.txt','utf-8');
 
   array = array.split("\n");
 
   for(let i = 0; i < array.length; i++) {
-    array[i] = array[i].replace("\r",'').split(' ');
+    array[i] = array[i].replace("\r",'').split(' ').map(el => +el);
     
   }
   return array;
 }
 
-const arrFromFile = getDataFromFile();
-const arrRand = getRandomArray();
 
-function game(arr) {
+// создаем игру
+function createGame(arr) {
     
     for(let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr[i].length; j++) {
@@ -76,5 +79,21 @@ function game(arr) {
     return arr;
 }
 
-let timeid = setInterval(() => game(arrFromFile) ,1000);
-//  console.log(countNeighbors(arrSmall,0,1));
+//задаю ввод и вывод из консоли
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// задание условий вывода поля ( рандомное или из файла )
+rl.question('Введите 1 для рандомной генерации поля.\nВведите 2 для получения поля из файла.\n ', (answer) => {
+  if(answer === '1') {
+    answer = setInterval(() => createGame(arrRand) ,1000);
+  } else if (answer === '2') {
+    answer = setInterval(() => createGame(arrFromFile) ,1000);
+  } else answer = 'неверный вариант'
+
+  console.log(answer);
+
+  rl.close();
+});
